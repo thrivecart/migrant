17ce798638853b6aabaf58c652249c66d7074be6
Joe Hart <joe@fluxoft.com>
2015-02-20 10:52:18 -0500

    Better integer test

--- src/MigrationFolder.php
+++ src/MigrationFolder.php
@@ -38,7 +38,7 @@ class MigrationFolder {
 		$hash = [];
 		foreach ($files as $file) {
 			list ($revision,) = explode('_', $file, 2);
-			if (is_int($revision) && (substr($file, -4) === '.sql')) {
+			if (ctype_digit(strval($revision)) && (substr($file, -4) === '.sql')) {
 				$hash[$revision]  = $file;
 			}
 		}
